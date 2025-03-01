af23603a37eae21ac5bf95dd5745d06793f1b0f5
Saurabh Chaudhary <ch.saurabh75@gmail.com>
2025-03-01 21:58:06 +0530

    final_commit

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-def count_characters(book):
-    with open(book) as f:
-        file_contents = f.read().lower()  # Convert to lowercase
-        char_count = {}
-        
-        for char in file_contents:
-            if char in char_count:
-                char_count[char] += 1
-            else:
-                char_count[char] = 1
-        
-        return char_count
-    
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+import sys
+
 
 def main():
-    book = "./books/frankenstein.txt"
-    character_counts = count_characters(book)
-    print(character_counts)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
 
 main()
